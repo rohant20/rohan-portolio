fcae4cd45ae0d24fea059401ed2eefd4c52afb92
Rohan Talagadadeevi <59935451+rohant20@users.noreply.github.com>
2024-05-24 23:59:48 -0400

    Finalized Website as of 5/24/2024
    Cleaned up formatting, breakpoint, and mobile display styling.  Finished filling in content for projects page, still need to fill in content for BLS project.

--- index.js
+++ index.js
@@ -14,26 +14,71 @@ let titleText = document.querySelector(".titleText");
 let headShot = document.querySelector(".headShot");
 let pattern = document.querySelector(".pattern");
 let panel2 = document.querySelector(".panel2");
+
+let card = document.querySelector("#homeCard");
+let cardHolder = document.querySelector("#cardHolder");
+
 let cardText = [
     "Get to know me! Read a little about my background and my journey that led me to where I am now!",
     "Check out some of my work!  Starting from my first projects, all the way to my most recent work!",
     "Don't be shy!  I'm eager to learn and work in Software Development, so reach out if you'd like to chat!"
 ]
 
+let cardImgSrc = [
+    "assets/Onstage.JPG",
+    "assets/projectCover.JPG",
+    "assets/contactCover.jpg"
+]
 
-const cardComp = {
-    title: document.getElementById("cardTitle"),
-    text: document.getElementById("cardText"),
-    image: document.getElementById("cardImg")
+
+class cardComp {
+
+    constructor() {
+        this.title = document.getElementById("cardTitle");
+        this.text = document.getElementById("cardText");
+        this.image = document.getElementById("cardImg");
+    }
+
+
+    resetComps() {
+        this.title = document.getElementById("cardTitle");
+        this.text = document.getElementById("cardText");
+        this.image = document.getElementById("cardImg");
+    }
+
+    getTitle() {
+        return this.title;
+    }
+
+    getText() {
+        return this.text;
+    }
+
+    getImage() {
+        return this.image;
+    }
 }
 
+const cardComponents = new cardComp();
 
 //Card Changing
 function hoverlinkFunction() {
     for (let i = 0; i < nodeList.length; i++) {
         nodeList[i].addEventListener("mouseover", function changeCard() {
-            cardComp.title.innerHTML = nodeList[i].children[0].innerHTML;
-            cardComp.text.innerHTML = cardText[i];
+            cardComponents.getTitle().innerHTML = nodeList[i].children[0].innerHTML;
+            cardComponents.getText().innerHTML = cardText[i];
+
+            //Switching between video and photo for the card
+            // if (i == 1 || i == 2) {
+            //     card.insertAdjacentHTML("afterbegin", "<video id='cardImg' src='' autoplay loop class='card-img-top cardStyle'></video>");
+            //     cardComp.image.remove();
+            // } else {
+            //     card.insertAdjacentHTML("afterbegin", "<img id='cardImg' src='' class='card-img card-img-top cardStyle' alt=''>");
+            //     cardComp.image.remove();
+            // }
+            // cardComp.image = document.querySelector("#cardImg");
+
+            cardComponents.getImage().setAttribute("src", cardImgSrc[i]);
         })
     }
 }
@@ -41,7 +86,6 @@ function hoverlinkFunction() {
 
 
 function changeStyle(w, component, orig, mod, bp) {
-    console.log(component);
     if (w < bp) {
         component.classList.add(mod);
         component.classList.remove(orig);
@@ -88,6 +132,14 @@ window.onresize = function () {
         changeStyle(width, titleText, 'titleText', 'adjustedTitleText', 768);
         changeStyle(width, headShot, 'headShot', 'adjustedHeadShot', 768);
         changeStyle(width, pattern, 'pattern', 'adjustedPattern', 768);
+
+        if (width <= 768) {
+            cardHolder.innerHTML = "";
+        } else if (width > 768) {
+            cardHolder.innerHTML = "<div id='homeCard' class='card'><img id='cardImg' src='assets/onStage.JPG' class='card-img card-img-top cardStyle' alt=''><div class='card-body'><h5 id='cardTitle' class='card-title'>About Me</h5><p id='cardText' class='card-text'>Get to know me! Read a little about my background and myjourney that led me to where I am now!</p></div></div>";
+            card = document.querySelector("#homeCard");
+            cardComponents.resetComps();
+        }
     } else if (pathname == "/aboutUs.html") {
         width = window.innerWidth;
         height = window.innerHeight;
@@ -105,10 +157,14 @@ window.addEventListener("load", myInit, true);
 function myInit() {
     pathname = window.location.pathname;
 
-    if (pathname == "/index.html") {
+    if (pathname == "/index.html" || pathname == "/") {
         changeStyle(width, pattern, 'pattern', 'adjustedPattern', 768);
         changeStyle(width, headShot, 'headShot', 'adjustedHeadShot', 768);
         changeStyle(width, titleText, 'titleText', 'adjustedTitleText', 768);
+
+        if (width <= 768) {
+            cardHolder.innerHTML = "";
+        }
         hoverlinkFunction();
         //implement the changing of the landing page card hover for smaller screens
     } else if (pathname == "/aboutUs.html") {
@@ -117,5 +173,5 @@ function myInit() {
 
 
 
-
 }
+
